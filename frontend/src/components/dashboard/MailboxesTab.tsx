d0bed80388defd598976ee46abbee25b71b16346
Blackx16 <chandraveer1601@gmail.com>
2026-07-23 01:16:53 +0530

    style: update Outlook and Gmail SVG logos with high resolution official brand assets

--- frontend/src/components/dashboard/MailboxesTab.tsx
+++ frontend/src/components/dashboard/MailboxesTab.tsx
@@ -5,22 +5,58 @@ import { Account } from "../../types/dashboard";
 /* ---- Official brand logos as inline SVGs ---- */
 function MicrosoftOutlookLogo({ className = "" }: { className?: string }) {
   return (
-    <svg className={className} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path d="M12 2L2 6v12l10 4 10-4V6L12 2z" fill="#0078D4" opacity="0.15" />
-      <path d="M22 8.5V17l-6 2.5V6l6 2.5z" fill="#0078D4" />
-      <path d="M16 6v13.5l-6 2.5V3.5L16 6z" fill="#0078D4" opacity="0.7" />
-      <path d="M10 3.5V22l-8-3.2V6.7L10 3.5z" fill="#0078D4" />
-      <ellipse cx="6" cy="12.5" rx="3" ry="3.5" fill="white" />
+    <svg className={className} viewBox="-3.2 -3.2 38.40 38.40" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <rect x="10" y="2" width="20" height="28" rx="2" fill="#1066B5"></rect>
+      <rect x="10" y="2" width="20" height="28" rx="2" fill="url(#paint0_linear_87_7742)"></rect>
+      <rect x="10" y="5" width="10" height="10" fill="#32A9E7"></rect>
+      <rect x="10" y="15" width="10" height="10" fill="#167EB4"></rect>
+      <rect x="20" y="15" width="10" height="10" fill="#32A9E7"></rect>
+      <rect x="20" y="5" width="10" height="10" fill="#58D9FD"></rect>
+      <mask id="mask0_87_7742" style={{ maskType: "alpha" }} maskUnits="userSpaceOnUse" x="8" y="14" width="24" height="16">
+        <path d="M8 14H30C31.1046 14 32 14.8954 32 16V28C32 29.1046 31.1046 30 30 30H10C8.89543 30 8 29.1046 8 28V14Z" fill="url(#paint1_linear_87_7742)"></path>
+      </mask>
+      <g mask="url(#mask0_87_7742)">
+        <path d="M32 14V18H30V14H32Z" fill="#135298"></path>
+        <path d="M32 30V16L7 30H32Z" fill="url(#paint2_linear_87_7742)"></path>
+        <path d="M8 30V16L33 30H8Z" fill="url(#paint3_linear_87_7742)"></path>
+      </g>
+      <path d="M8 12C8 10.3431 9.34315 9 11 9H17C18.6569 9 20 10.3431 20 12V24C20 25.6569 18.6569 27 17 27H8V12Z" fill="#000000" fillOpacity="0.3"></path>
+      <rect y="7" width="18" height="18" rx="2" fill="url(#paint4_linear_87_7742)"></rect>
+      <path d="M14 16.0693V15.903C14 13.0222 11.9272 11 9.01582 11C6.08861 11 4 13.036 4 15.9307V16.097C4 18.9778 6.07278 21 9 21C11.9114 21 14 18.964 14 16.0693ZM11.6424 16.097C11.6424 18.0083 10.5665 19.1579 9.01582 19.1579C7.46519 19.1579 6.37342 17.9806 6.37342 16.0693V15.903C6.37342 13.9917 7.44937 12.8421 9 12.8421C10.5348 12.8421 11.6424 14.0194 11.6424 15.9307V16.097Z" fill="white"></path>
+      <defs>
+        <linearGradient id="paint0_linear_87_7742" x1="10" y1="16" x2="30" y2="16" gradientUnits="userSpaceOnUse">
+          <stop stopColor="#064484"></stop>
+          <stop offset="1" stopColor="#0F65B5"></stop>
+        </linearGradient>
+        <linearGradient id="paint1_linear_87_7742" x1="8" y1="26.7692" x2="32" y2="26.7692" gradientUnits="userSpaceOnUse">
+          <stop stopColor="#1B366F"></stop>
+          <stop offset="1" stopColor="#2657B0"></stop>
+        </linearGradient>
+        <linearGradient id="paint2_linear_87_7742" x1="32" y1="23" x2="8" y2="23" gradientUnits="userSpaceOnUse">
+          <stop stopColor="#44DCFD"></stop>
+          <stop offset="0.453125" stopColor="#259ED0"></stop>
+        </linearGradient>
+        <linearGradient id="paint3_linear_87_7742" x1="8" y1="23" x2="32" y2="23" gradientUnits="userSpaceOnUse">
+          <stop stopColor="#259ED0"></stop>
+          <stop offset="1" stopColor="#44DCFD"></stop>
+        </linearGradient>
+        <linearGradient id="paint4_linear_87_7742" x1="0" y1="16" x2="18" y2="16" gradientUnits="userSpaceOnUse">
+          <stop stopColor="#064484"></stop>
+          <stop offset="1" stopColor="#0F65B5"></stop>
+        </linearGradient>
+      </defs>
     </svg>
   );
 }
 
 function GmailLogo({ className = "" }: { className?: string }) {
   return (
-    <svg className={className} viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-      <path d="M2 6l10 7L22 6v12a2 2 0 01-2 2H4a2 2 0 01-2-2V6z" fill="#EA4335" opacity="0.15" />
-      <path d="M22 6l-10 7L2 6" stroke="#EA4335" strokeWidth="2" fill="none" strokeLinecap="round" strokeLinejoin="round" />
-      <rect x="2" y="6" width="20" height="12" rx="2" stroke="#EA4335" strokeWidth="1.5" fill="none" />
+    <svg className={className} viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
+      <path d="M16.58,19.1068l-12.69-8.0757A3,3,0,0,1,7.1109,5.97l9.31,5.9243L24.78,6.0428A3,3,0,0,1,28.22,10.9579Z" fill="#ea4435"></path>
+      <path d="M25.5,5.5h4a0,0,0,0,1,0,0v18a3,3,0,0,1-3,3h0a3,3,0,0,1-3-3V7.5a2,2,0,0,1,2-2Z" fill="#00ac47" transform="translate(53.0001 32.0007) rotate(180)"></path>
+      <path d="M29.4562,8.0656c-.0088-.06-.0081-.1213-.0206-.1812-.0192-.0918-.0549-.1766-.0823-.2652a2.9312,2.9312,0,0,0-.0958-.2993c-.02-.0475-.0508-.0892-.0735-.1354A2.9838,2.9838,0,0,0,28.9686,6.8c-.04-.0581-.09-.1076-.1342-.1626a3.0282,3.0282,0,0,0-.2455-.2849c-.0665-.0647-.1423-.1188-.2146-.1771a3.02,3.02,0,0,0-.24-.1857c-.0793-.0518-.1661-.0917-.25-.1359-.0884-.0461-.175-.0963-.267-.1331-.0889-.0358-.1837-.0586-.2766-.0859s-.1853-.06-.2807-.0777a3.0543,3.0543,0,0,0-.357-.036c-.0759-.0053-.1511-.0186-.2273-.018a2.9778,2.9778,0,0,0-.4219.0425c-.0563.0084-.113.0077-.1689.0193a33.211,33.211,0,0,0-.5645.178c-.0515.022-.0966.0547-.1465.0795A2.901,2.901,0,0,0,23.5,8.5v5.762l4.72-3.3043a2.8878,2.8878,0,0,0,1.2359-2.8923Z" fill="#ffba00"></path>
+      <path d="M5.5,5.5h0a3,3,0,0,1,3,3v18a0,0,0,0,1,0,0h-4a2,2,0,0,1-2-2V8.5a3,3,0,0,1,3-3Z" fill="#4285f4"></path>
+      <path d="M2.5439,8.0656c.0088-.06.0081-.1213.0206-.1812.0192-.0918.0549-.1766.0823-.2652A2.9312,2.9312,0,0,1,2.7426,7.32c.02-.0475.0508-.0892.0736-.1354A2.9719,2.9719,0,0,1,3.0316,6.8c.04-.0581.09-.1076.1342-.1626a3.0272,3.0272,0,0,1,.2454-.2849c.0665-.0647.1423-.1188.2147-.1771a3.0005,3.0005,0,0,1,.24-.1857c.0793-.0518.1661-.0917.25-.1359A2.9747,2.9747,0,0,1,4.3829,5.72c.089-.0358.1838-.0586.2766-.0859s.1853-.06.2807-.0777a3.0565,3.0565,0,0,1,.357-.036c.076-.0053.1511-.0186.2273-.018a2.9763,2.9763,0,0,1,.4219.0425c.0563.0084.113.0077.169.0193a2.9056,2.9056,0,0,1,.286.0888,2.9157,2.9157,0,0,1,.2785.0892c.0514.022.0965.0547.1465.0795a2.9745,2.9745,0,0,1,.3742.21A2.9943,2.9943,0,0,1,8.5,8.5v5.762L3.78,10.9579A2.8891,2.8891,0,0,1,2.5439,8.0656Z" fill="#c52528"></path>
     </svg>
   );
 }
